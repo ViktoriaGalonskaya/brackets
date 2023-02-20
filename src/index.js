8f46f5cce93e0f25e8c71a6b4da440440d664542
victoriagalonskaya <victoriagalonskaya1997@mail.ru>
2023-02-20 19:52:21 +0300

    feat: add brackets task solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let pairsBrack = Object.fromEntries(bracketsConfig);
+
+  function getPair() {
+    if (i >= str.length) return false;
+    const close = pairsBrack[str[i]];
+    if (!close) return false;
+    for (i = i + 1; i < str.length; i++) {
+      if (str[i] === close) return true; 
+      if (!getPair()) return false; 
+    }
+    return false; 
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    if (!getPair()) return false; 
+  }
+  return true; 
 }
